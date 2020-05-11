6182fb6cd3882afc2c81eedcc7d7e0ec07bda0a1
于是乎 <1466496096@qq.com>
2020-05-11 09:48:12 +0800

    添加项目文件。

---
+++ Day18/Controllers/RegController.cs
@@ -0,0 +1,105 @@
+﻿using CacheManager.Core;
+using Day18.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day18.Controllers
+{
+    public class RegController : Controller
+    {
+
+        string strPath = string.Empty;
+        // GET: Reg
+        public ActionResult Index()
+        {
+            var cache = CacheFactory.Build<string>(settings =>
+            {
+                settings
+                    //.WithSystemRuntimeCacheHandle()
+                    //.And
+                    .WithRedisConfiguration("redis", config =>
+                    {
+                        config.WithAllowAdmin()
+                            .WithDatabase(0)
+                            .WithEndpoint("localhost", 6379);
+                    })
+                    .WithMaxRetries(1000)
+                    .WithRetryTimeout(100)
+                    .WithRedisBackplane("redis")
+                    .WithRedisCacheHandle("redis", true);
+            });
+            // registerModel
+            var result = cache.Get("registerModel");
+            RegisterModel model = JsonConvert.DeserializeObject<RegisterModel>(result);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel registerModel, HttpPostedFileBase headImg)
+        {
+            UploadHeadImg(headImg);
+            registerModel.ImagePath = strPath;
+            registerModel.Fav = Request.Form["Fav"];
+            RedisSample(registerModel);
+
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        public void UploadHeadImg(HttpPostedFileBase headImg)
+        {
+            //bool isSuccess = true;
+            try
+            {
+                var dateDir = DateTime.Now.ToString("yyyy-MM-dd");
+                var spath = Path.Combine(HttpContext.Server.MapPath("~/"), dateDir);
+                //var path = HttpContext.Server.MapPath(spath);
+                if (!Directory.Exists(spath))
+                {
+                    Directory.CreateDirectory(spath);
+                }
+
+                strPath = Path.Combine(spath, headImg.FileName);
+                headImg.SaveAs(strPath);
+            }
+            catch (Exception ex)
+            {
+                //isSuccess = false;
+            }
+
+            //return isSuccess;
+        }
+
+
+        private static void RedisSample(RegisterModel registerModel)
+        {
+            var cache = CacheFactory.Build<string>(settings =>
+            {
+                settings
+                    //.WithSystemRuntimeCacheHandle()
+                    //.And
+                    .WithRedisConfiguration("redis", config =>
+                    {
+                        config.WithAllowAdmin()
+                            .WithDatabase(0)
+                            .WithEndpoint("localhost", 6379);
+                    })
+                    .WithMaxRetries(1000)
+                    .WithRetryTimeout(100)
+                    .WithRedisBackplane("redis")
+                    .WithRedisCacheHandle("redis", true);
+            });
+
+            //cache.Add("test", 123456);
+            cache.Clear();
+            cache.Add(nameof(registerModel), JsonConvert.SerializeObject(registerModel));
+
+            //cache.Update("test", p => p + 1);
+
+
+        }
+    }
+}
---
+++ Day18/Models/RegisterModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Day18.Models
+{
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+
+        public SexEnum Sex { get; set; }
+
+
+        public int Age { get; set; }
+
+
+        public StudyExperience StudyExp { get; set; }
+
+
+        public string ImagePath { get; set; }
+
+
+        public string Motto { get; set; }
+
+
+        public string Fav { get; set; }
+
+        public string Fav1 { get; set; }
+
+        public string Fav2 { get; set; }
+
+        public string Fav3 { get; set; }
+    }
+
+
+    public enum SexEnum
+    {
+        [Description("女")]
+        Female = 1,
+
+        [Description("男")]
+        Male = 0
+    }
+
+    public enum StudyExperience
+    {
+        [Description("本科")]
+        Bachelor = 0,
+
+        [Description("大专")]
+        College = 1,
+
+        [Description("高中")]
+        HighSchool = 2
+
+    }
+
+
+}
